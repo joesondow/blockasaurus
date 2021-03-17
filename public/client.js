6b69dbe011219a53e47fe8597e7200536b52a640
Joe Sondow <joesondow@users.noreply.github.com>
2021-03-17 00:22:45 -0700

    In progress, adding ajax, higher block count limit, reuse session

--- public/client.js
+++ public/client.js
@@ -1,4 +1,121 @@
 // client-side js
 // run by the browser each time your view template is loaded
 
-console.log('hello world :o');
+var logout = function() {
+    sessionStorage.removeItem('accessTokenPair');
+    jQuery('#twitterUser').html('');
+};
+
+(function() {
+  "use strict";
+  
+   $.urlParam = function(name){
+      var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+      if (results == null){
+         return null;
+      }
+      else{
+         return results[1] || null;
+      }
+  }
+  
+  var validateAccessTokenPair = function(accessTokenPair) {
+    
+    if (accessTokenPair) {
+      jQuery.ajax({
+        method: "POST",
+        url: '/validate-user',
+        data: {
+          accessTokenPair: accessTokenPair
+        },
+        success: function(user) {
+          login(accessTokenPair, user);
+        },
+        error: function(data) {
+          logout();
+        }
+      });
+    }
+  };
+  
+  // Look for token pair
+  var accessTokenPair;
+  if (window.blockasaurus && window.blockasaurus.accessTokenPair) {
+    accessTokenPair = window.blockasaurus.accessTokenPair;
+  }
+  if (!accessTokenPair) {
+    accessTokenPair = sessionStorage.getItem('accessTokenPair');
+  }
+  if (!accessTokenPair) {
+    console.log('access token pair not found');
+  }
+  
+  validateAccessTokenPair(accessTokenPair);  
+    
+  var login = function(accessTokenPair, user) {
+    sessionStorage.setItem('accessTokenPair', accessTokenPair);
+    jQuery('#input_logged_in').val(true);
+    
+    // If url param logged_in is not present, add it.
+    if (jQuery.urlParam('logged_in') === 'true') {
+      // console.log('url param found');
+      // Show the user's profile pic
+      var profilePicUrlHttps = user.profile_image_url_https;
+      var screenName = user.screen_name;
+      var displayName = user.name;
+      jQuery('#twitterUser').html('Logged in: @'+ screenName + ' <a href="javascript:logout()">Logout</a>');
+    } else {
+      // console.log('url param NOT found');
+      var sep = window.location.href.indexOf('?') > -1 ? '&' : '?';
+      var url = window.location.href + sep + 'logged_in=true';
+      window.location.replace(url);
+    }
+  };
+  
+  var checkFriendships = function() {
+    jQuery.ajax({
+      method: "POST",
+      url: "/check-friendships",
+      data: {
+        screenNames: window.blockasaurus.screenNamesToBlock.join(","),
+        accessTokenPair: accessTokenPair
+      },
+      success: function(data) {
+        // data is an array of objects.
+
+        for (var i = 0; i < data.length; i++) {
+          var friendship = data[i];
+          var screenName = friendship.screen_name;
+          var idStr = friendship.id_str;
+          var connections = friendship.connections;
+          if (connections && connections.some(conn => conn === "blocking")) {
+            console.log("Already blocked " + screenName);
+            jQuery('#td_status_' + screenName).html('Already blocked');
+          } else {
+            console.log("Not yet blocked " + screenName);
+          }
+        }
+      },
+      error: function(data) {}
+    });
+  };
+
+  if (window.blockasaurus.screenNamesToBlock) {
+    checkFriendships();
+  }
+  
+  // Get the Twitter credentials.
+//   var trex = window.blockasaurus;
+//   if (trex) {
+//     var accessToken = trex.twitterTokens.access_token_key;
+//     var accessTokenSecret = trex.twitterTokens.access_token_secret;
+//     var screenNamesToBlock = trex.screenNamesToBlock;
+//     var screenNameStatuses = {};
+
+//     for (var k = 0; k < screenNamesToBlock.length; k++) {
+//       var handle = screenNamesToBlock[k];
+//       screenNameStatuses[handle] = {status: "tbd"};
+//     }
+//   }
+  
+})();
--- server.js
+++ server.js
@@ -2,14 +2,19 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var OAuth = require('oauth');
+var express = require("express");
+var OAuth = require("oauth");
 
 var app = express();
 
 // fill with keys provided by Twitter
 const CONSUMER_KEY = process.env.CONSUMER_KEY;
 const CONSUMER_SECRET = process.env.CONSUMER_SECRET;
+
+// Hard-coded temporarily during development
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+
 // enable this through twitter's app management panel
 const CALLBACK_SERVER = process.env.CALLBACK_SERVER;
 const CALLBACK_RESOURCE = '/access-token';
@@ -33,35 +38,33 @@ function parse_blocklist(blocklist) {
   var tokens = blocklist.split(/[^_A-Za-z0-9]/);
   var screenNamesSet = new Set();
   var screenNames = [];
-  var skipTokens = ["https", "http", "twitter", "com", "status", ]
-  // var tokens = blocklist.split(/[ ,\n]/);
-  // console.log(tokens.length);
-  tokens.forEach((token) => {
-    // console.log("--" + token + "--");
-    if (token.length >= 4 && token.length <= 15 && !skipTokens.includes(token)) {
+  var skipTokens = ["https", "http", "twitter", "com", "status"];
+  tokens.forEach(token => {
+    if (
+      token.length >= 4 &&
+      token.length <= 15 &&
+      !skipTokens.includes(token.toLowerCase())
+    ) {
       screenNamesSet.add(token);
     }
   });
-  screenNamesSet.forEach((screenname) => {
+  screenNamesSet.forEach(screenname => {
     screenNames.push(screenname);
-  });
-  screenNames.forEach((screenName) => {
-    // console.log("++" + screenName + "++");
   });
   return screenNames;
 }
 
-function eval_report(report){
-  if (typeof report != 'undefined') {
-    return true;
-  } else {
-    return false;
-  }
-}
+// function eval_report(report) {
+//   if (typeof report != "undefined") {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
-function blockAll(client, accounts, report=false, cursor=null) {
-  accounts.forEach((screen_name) => {
-    if (report){
+function blockAll(client, accounts, report = false, cursor = null) {
+  accounts.forEach(screen_name => {
+    if (report) {
       report_and_block(client, screen_name);
     } else {
       block(client, screen_name);
@@ -71,56 +74,45 @@ function blockAll(client, accounts, report=false, cursor=null) {
 
 function report_and_block(client, screen_name) {
   client.post(
-    'users/report_spam.json',
-    {screen_name: screen_name, perform_block: true},
-    function(errors, response) {
-      if (errors) {
-        // errors.forEach((error) => {
-        //   console.log(error['message']);
-        console.log(errors);
-        // });
-      }
-      else {
-        console.log('user @' + screen_name + ' has been blocked!');
-      }
-    }
-  );
-}
-
-function block(client, screen_name){
-  client.post(
-    'blocks/create.json',
-    {screen_name: screen_name},
-    function(errors, response) {
-      if (errors) {
-        console.log(errors);
-        // errors.forEach((error) => {
-        //   console.log(error['message']);
-        // });
+    "users/report_spam.json",
+    { screen_name: screen_name, perform_block: true },
+    function(error, response) {
+      if (error) {
+        console.log(error);
       } else {
-        console.log('user @' + screen_name + ' has been blocked!');
+        console.log("blocked and reported @" + screen_name);
       }
     }
   );
 }
 
+function block(client, screen_name) {
+  client.post("blocks/create.json", { screen_name: screen_name }, function(
+    error,
+    response
+  ) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("blocked @" + screen_name);
+    }
+  });
+}
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/index.html");
 });
-
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/faq', function(request, response) {
-  response.sendFile(__dirname + '/views/faq.html');
+app.get("/faq", function(request, response) {
+  response.sendFile(__dirname + "/views/faq.html");
 });
 
-
-app.post('/block', function(request, response) {
+app.post("/block", function(request, response) {
   var accounts = parse_blocklist(request.body.blocklist);
   // var tweet_url = request.body.tweet_url;
-  var report = eval_report(request.body.report);
+  // var report = eval_report(request.body.report);
   //Starts twitter and authenticates
   var client = new Twitter({
     consumer_key: request.body.consumer_key,
@@ -128,11 +120,12 @@ app.post('/block', function(request, response) {
     access_token_key: request.body.access_token,
     access_token_secret: request.body.access_token_secret
   });
-  blockAll(client, accounts, report);
+  blockAll(client, accounts);
   client = null;
-  ejs.renderFile(__dirname + '/views/block.ejs', 
-  {},  // {tweet_url: tweet_url}, {},
-    function(err, str){
+  ejs.renderFile(
+    __dirname + "/views/block.ejs",
+    {},
+    function(err, str) {
       response.send(str);
     }
   );
@@ -140,74 +133,191 @@ app.post('/block', function(request, response) {
 
 function get_twitter_oauth(blocklist) {
   return new OAuth.OAuth(
-    'https://api.twitter.com/oauth/request_token',
-    'https://api.twitter.com/oauth/access_token',
+    "https://api.twitter.com/oauth/request_token",
+    "https://api.twitter.com/oauth/access_token",
     CONSUMER_KEY,
     CONSUMER_SECRET,
-    '1.0A',
-    CALLBACK_URL + '?blocklist=' + encodeURIComponent(blocklist),
-    'HMAC-SHA1'
+    "1.0A",
+    CALLBACK_URL + "?blocklist=" + encodeURIComponent(blocklist),
+    "HMAC-SHA1"
   );
 }
 
-app.post('/request-token', function(request, response) {
-  var client = new Twitter({
-    consumer_key: CONSUMER_KEY,
-    consumer_secret: CONSUMER_SECRET,
-  });
+app.post("/request-token", function(request, response) {
+
   var blocklist = request.body.blocklist;
-  // request an unauthorized Request Token from twitter (OAuth1.0 - 6.1)
-  var oa = get_twitter_oauth(blocklist);
-  oa.getOAuthRequestToken(function(error, request_token, request_secret, results) {
-    if (!error) {
-      // send the user to authorize the Request Token (OAuth1.0 - 6.2)
-      response.redirect('https://api.twitter.com/oauth/authorize?oauth_token='+request_token);
-    }
-    else {
-      response.send(error);
-    }
-  });
+  var uriEncodedBlocklist = encodeURIComponent(blocklist);
+  
+  if (request.body.logged_in === 'true') {
+    // Assume creds are in sessionStorage and client JS can log in already, 
+    // so redirect to access-token
+    response.redirect(CALLBACK_RESOURCE + '?logged_in=true&blocklist=' + uriEncodedBlocklist);
+  } else {
+    // Logged out, need to authenticate
+    var client = new Twitter({
+      consumer_key: CONSUMER_KEY,
+      consumer_secret: CONSUMER_SECRET
+    });
+    // request an unauthorized Request Token from twitter (OAuth1.0 - 6.1)
+    var oa = get_twitter_oauth(blocklist);
+    oa.getOAuthRequestToken(function(
+      error,
+      request_token,
+      request_secret,
+      results
+    ) {
+      if (!error) {
+        // send the user to authorize the Request Token (OAuth1.0 - 6.2)
+        response.redirect(
+          "https://api.twitter.com/oauth/authorize?oauth_token=" + request_token
+        );
+      } else {
+        response.send(error);
+      }
+    });
+  }
 });
+
+function renderBlockEjs(response, data) {
+  ejs.renderFile(
+      __dirname + "/views/block.ejs",
+      data,
+      function(err, str) {
+        response.send(str);
+      }
+    );
+}
 
 app.get(CALLBACK_RESOURCE, function(request, response) {
-  // get the authorized Request Token from the GET parameters
-  var request_token = request.query.oauth_token;
-  var oauth_verifier = request.query.oauth_verifier;
   var blocklist = request.query.blocklist;
-  // console.log("__"+blocklist+"__");
   var accounts = parse_blocklist(blocklist);
-  // console.log("__"+accounts.length+"__");
-  var report = eval_report(request.body.reportauth);
+  console.log(CALLBACK_RESOURCE + ' ' + blocklist);
 
-  var oa = get_twitter_oauth(accounts);
-  // exchange the authorized Request Token for an Access Token (OAuth1.0 - 6.3)
-  oa.getOAuthAccessToken(request_token, null, oauth_verifier, function(error, access_token, access_token_secret, results) {
-    if (!error) {
-      var client = new Twitter({
-        consumer_key: CONSUMER_KEY,
-        consumer_secret: CONSUMER_SECRET,
-        access_token_key: access_token,
-        access_token_secret: access_token_secret
-      });
-      try {
-        blockAll(client, accounts, report);
+  if (request.query.logged_in === 'true') {
+    renderBlockEjs(response, { accounts: accounts, accessTokenPair: '' });
+  } else {
+    // get the authorized Request Token from the GET parameters
+    var request_token = request.query.oauth_token;
+    var oauth_verifier = request.query.oauth_verifier;
+
+    // This has never worked in poop-blocker or Blockasaurus because reportauth is not the name of the form input.
+    // I might fix and test this feature in the future. For now, it's meaningless.
+    // var report = eval_report(request.body.reportauth);
+
+    var oa = get_twitter_oauth(accounts);
+    // exchange the authorized Request Token for an Access Token (OAuth1.0 - 6.3)
+    oa.getOAuthAccessToken(request_token, null, oauth_verifier, function(
+      error,
+      access_token,
+      access_token_secret,
+      results
+    ) {
+      if (!error) {
+        var client = new Twitter({
+          consumer_key: CONSUMER_KEY,
+          consumer_secret: CONSUMER_SECRET,
+          access_token_key: access_token,
+          access_token_secret: access_token_secret
+        });
+        var accessTokenPair = access_token + "," + access_token_secret;
+        // try {
+        //   blockAll(client, accounts);
+        // } catch (error) {
+        //   response.send(error);
+        //   return;
+        // }
+        client = null;
+        console.log('accessTokenPair ' + accessTokenPair);
+        renderBlockEjs(response, { accounts: accounts, accessTokenPair: accessTokenPair });
       }
-      catch (error) {
+      else {
+        console.log(error)
         response.send(error);
-        return;
       }
-      client = null;
-
-      ejs.renderFile(__dirname + '/views/block.ejs', 
-        {accounts: accounts}, 
-        function(err, str) { response.send(str); }
-      );
-    }
-    else {
-      response.send(error);
-    }
-  });
+    });
+  }
+  
+  
 });
+
+function makeClient(accessTokenPair) {
+  
+  var accessToken, accessTokenSecret;
+  var tokens = accessTokenPair.split(',');
+  if (tokens.length === 2) {
+    accessToken = tokens[0];
+    accessTokenSecret = tokens[1];
+  } else {
+    throw 'Invalid credentials';
+  }
+  
+  return new Twitter({
+    consumer_key: CONSUMER_KEY,
+    consumer_secret: CONSUMER_SECRET,
+    access_token_key: accessToken,
+    access_token_secret: accessTokenSecret
+  });
+}
+
+
+app.post("/check-friendships", function(request, response) {
+  var accessTokenPair = request.body.accessTokenPair;
+  
+  
+  // var access_token = request.body.access_token;
+  // var access_token_secret = request.body.access_token_secret;
+  var screenNamesCsv = request.body.screenNames;
+  
+  var client = makeClient(accessTokenPair);
+  try {
+    client.get(
+      "friendships/lookup",
+      { screen_name: screenNamesCsv },
+      function(error, friendships, resp) {
+        if (error) {
+          response.json(error);
+        } else {
+          response.json(friendships);
+        }
+      }
+    );
+  } catch (error) {
+    response.json(error);
+    return;
+  }
+  client = null;
+});
+
+app.post('/validate-user', function(request, response) {
+  var accessTokenPair = request.body.accessTokenPair;
+  console.log('validate-user accessTokenPair '+ accessTokenPair);
+  var client = makeClient(accessTokenPair);
+  try {
+    client.get(
+      'account/verify_credentials',
+      {},
+      function(error, user, resp) {
+        if (error) {
+          response.json(error);
+        } else {
+          response.json(user);
+        }
+      }
+    );
+  } catch (error) {
+    response.json(error);
+    return;
+  }
+  client = null;
+});
+
+
+
+// app.post("/block-one", function(request, response) {
+//   var access_token = request.body.access_token;
+//   var access_token_secret = request.body.access_token_secret;
+//   var target_account = request.body.target_account;
+// });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
